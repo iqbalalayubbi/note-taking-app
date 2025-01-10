5f7719a9c58766fb04717ae4dfaba5ed6f637716
iqbalalayubbi <iqbal.is415@gmail.com>
2025-01-10 21:47:58 +0700

    feat: responsive modal global component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -12,7 +12,7 @@ type Property = {
 const Modal = ({ title, isOpen, onClose, children, icon }: Property) => {
   return (
     isOpen && (
-      <main className="h-screen flex items-center px-4 absolute top-0 left-0 w-screen z-40">
+      <main className="h-screen flex items-center px-4 absolute top-0 left-0 right-0 ml-auto mr-auto w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-5/12  z-40">
         <section className="relative rounded-md flex flex-col items-center py-10 gap-4 w-full  bg-white z-20">
           <div className="flex flex-col justify-center items-center gap-5">
             {icon}

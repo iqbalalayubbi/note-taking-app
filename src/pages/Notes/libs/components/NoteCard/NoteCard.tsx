ec9dea268c872a8d453f30dd6407b98879b77e89
iqbalalayubbi <iqbal.is415@gmail.com>
2025-01-10 09:41:46 +0700

    feat: add hover effect in card component

--- src/pages/Notes/libs/components/NoteCard/NoteCard.tsx
+++ src/pages/Notes/libs/components/NoteCard/NoteCard.tsx
@@ -2,9 +2,9 @@ import { Icon } from "@iconify-icon/react/dist/iconify.mjs";
 
 const NoteCard = () => {
   return (
-    <section className="border-2 border-slate-300 rounded-md bg-pink-50 px-4 py-4">
+    <section className="border-2 border-slate-300 rounded-md bg-pink-50 px-4 py-4 shadow-md hover:shadow-none cursor-pointer transition-all duration-200 hover:bg-pink-100">
       <div className="flex gap-4 flex-col">
-        <h2 className="text-xl font-bold">Sample Note Title</h2>
+        <h2 className="text-xl font-bold text-black/80">Sample Note Title</h2>
         <p className="opacity-60">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis,
           aliquam fugit ratione eius eaque dolorum laboriosam esse vero eos
@@ -12,11 +12,11 @@ const NoteCard = () => {
         </p>
       </div>
       <div className="flex justify-end gap-4 mt-5">
-        <button className="rounded-full bg-white w-10 h-10 flex items-center justify-center">
-          <Icon icon="line-md:edit" />
+        <button className="rounded-full bg-blue-300 w-10 h-10 flex items-center justify-center shadow-md hover:bg-blue-500 transition-all duration-300">
+          <Icon icon="line-md:edit" className="text-white" />
         </button>
-        <button className="rounded-full bg-white w-10 h-10 flex items-center justify-center">
-          <Icon icon="material-symbols:delete-outline" />
+        <button className="rounded-full bg-red-300 w-10 h-10 flex items-center justify-center shadow-md hover:bg-red-500 transition-all duration-300">
+          <Icon icon="material-symbols:delete-outline" className="text-white" />
         </button>
       </div>
     </section>

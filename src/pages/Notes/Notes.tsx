2d50958ee06af322b7eb3a18bcc2c1419fdbc128
iqbalalayubbi <iqbal.is415@gmail.com>
2025-01-10 10:05:15 +0700

    feat: responsive notes pages

--- src/pages/Notes/Notes.tsx
+++ src/pages/Notes/Notes.tsx
@@ -5,8 +5,10 @@ const Notes = () => {
   return (
     <main>
       <Navbar />
-      <section className="mt-44 md:mt-32 px-4 grid grid-cols-1 gap-y-4">
+      <section className="mt-44 md:mt-32 px-4 grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-4 xl:grid-cols-3">
         <AddCard />
+        <NoteCard />
+        <NoteCard />
         <NoteCard />
       </section>
     </main>

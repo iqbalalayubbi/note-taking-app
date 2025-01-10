6015c741e17ed29696af85af54c83030daf092a5
iqbalalayubbi <iqbal.is415@gmail.com>
2025-01-10 07:38:34 +0700

    feat: add hover effect to button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,7 +7,7 @@ type Properties = {
 const Button = ({ className, label, type }: Properties) => {
   return (
     <button
-      className={`rounded-full bg-purple-500 py-3 font-bold text-white ${className}`}
+      className={`rounded-full bg-purple-500 py-3 font-bold text-white hover:bg-purple-600 transition-all duration-200 ${className}`}
       type={type}
     >
       {label}
